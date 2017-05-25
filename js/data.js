3ca3ef4b9ed8819ff16d47f8326cf06f20c91711
arshavskiy <pasha.websbee@gmail.com>
2017-05-25 06:10:02 +0300

    article selection added

--- js/data.js
+++ js/data.js
@@ -3,14 +3,16 @@ var dataBase = [];
 
 
 function rememberMySearchResolts(headline, snippet, web_url, pub_date) {
+
     var searchWord = $("input:last").val();
+    console.log(headline);
+
     dataBase[counter] = {
         headline: headline,
         snippet: snippet,
         web_url: web_url,
         pub_date: pub_date,
         search: searchWord,
-
     };
     counter++;
 }
